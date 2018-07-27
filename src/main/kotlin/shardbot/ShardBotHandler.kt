66eb057ed7cce08d070a51c46cde458f1c5541a9
Plasmoxy <sebohacker@gmail.com>
2018-07-27 23:27:49 +0200

    dist command mode

--- src/main/kotlin/shardbot/ShardBotHandler.kt
+++ src/main/kotlin/shardbot/ShardBotHandler.kt
@@ -6,11 +6,22 @@ import com.github.messenger4j.exception.MessengerIOException
 import com.github.messenger4j.send.MessagePayload
 import com.github.messenger4j.send.MessagingType
 import com.github.messenger4j.send.message.TextMessage
+import com.github.messenger4j.webhook.Event
 import io.javalin.Context
 import io.javalin.Handler
 import java.util.*
 
 class ShardBotHandler(private val messenger: Messenger) : Handler {
+	
+	fun process(event: Event, text: String): String = when(text) {
+		
+		"echo" -> "ok"
+		
+		"myId" -> event.senderId()
+		"botId" -> event.recipientId()
+		
+		else -> "Unknown command my dude"
+	}
 	
 	override fun handle(ctx: Context) {
 		println("[ShardBot] WEBHOOK TRIGGERED -> echo")
@@ -22,15 +33,18 @@ class ShardBotHandler(private val messenger: Messenger) : Handler {
 			if (event.isTextMessageEvent) {
 				val text = event.asTextMessageEvent().text()
 				
-				val textMessage = TextMessage.create(text)
+				val response = process(event, text)
+				
+				val textMessage = TextMessage.create(response)
 				val messagePayload = MessagePayload.create(senderId,
 						MessagingType.RESPONSE, textMessage)
 				
 				try {
 					messenger.send(messagePayload)
 				} catch (e: MessengerApiException) {
-					// Oops, something went wrong
+					println(e.message)
 				} catch (e: MessengerIOException) {
+					println(e.message)
 				}
 				
 			}
